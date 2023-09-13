116ac9bc5d7a04f8e552bc3cab6664d006326a99
oussama <freegiftcardishere@gmail.com>
2023-09-13 13:30:12 +0200

    test

---
+++ third/calcul.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    int a;
+    int b;
+    char op;
+
+    printf("\t CAL \t \n");
+
+    scanf("%d %c %d", &a , &op, &b);
+
+    if (op == '\'')
+        printf();
+}
