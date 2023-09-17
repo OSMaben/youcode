3d3ed9b6933edd84869bdf3aec2ffde55939b5e7
OSMBEN <118306730+OSMaben@users.noreply.github.com>
2023-09-17 19:49:26 +0200

    Add files via upload

---
+++ calculator/main.h
@@ -0,0 +1,18 @@
+#ifndef MAX_H
+#define MAX_H
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>  
+#include <unistd.h>   
+#include "main.h"
+
+void checker(int num);
+void fn_addition(float num1, float num2);
+void fn_siustraction(float num1 , float num2);
+void fn_multiplication(float num1, float num2);
+void fn_division(float num1, float num2);
+void fn_carree(float num1, float num2);
+void fn_racine_carree(float nb);
+void menu(void);
+
+#endif
